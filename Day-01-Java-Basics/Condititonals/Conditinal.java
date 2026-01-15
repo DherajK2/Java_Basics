b1ffec96885b84824c0daf80a3d0f8f1164b6fc1
DherajK2 <dherajkd2@gmail.com>
2026-01-15 21:16:21 +0530

    Added Comments to if else

--- Day-01-Java-Basics/Condititonals/Conditinal.java
+++ Day-01-Java-Basics/Condititonals/Conditinal.java
@@ -1,8 +1,10 @@
 
 import java.util.*;
+// A simple program demonstrating conditional statements in Java
 public class Conditinal {
     public static void main(String[] args) {
         int a =9;
+        // Check if a is less than 5 and modify its value accordingly
         if(a<5){
             System.out.println("a is less than 5");
             a=10;

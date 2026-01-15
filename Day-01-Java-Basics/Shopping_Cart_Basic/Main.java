04af68557c474da22075a86b15b8072db0cfbdf4
DherajK2 <dherajkd2@gmail.com>
2026-01-15 21:03:29 +0530

    Add Day 1 Java Basics Shopping Cart code with comments

--- Day-01-Java-Basics/Shopping_Cart_Basic/Main.java
+++ Day-01-Java-Basics/Shopping_Cart_Basic/Main.java
@@ -1,30 +1,33 @@
-package Shopping_Cart_Basic;
-
-import java.util.*;
+import java.util.*;  // Import Scanner for user input
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Price of Pen");
+        Scanner sc = new Scanner(System.in);  // Create Scanner object
+
+        // Take input for item prices
+        System.out.println("Enter the Price of Pen: ");
         double pen = sc.nextDouble();
-         System.out.println("Enter the Price of Pencil");
+
+        System.out.println("Enter the Price of Pencil: ");
         double pencil = sc.nextDouble();
-         System.out.println("Enter the Price of Book");
+
+        System.out.println("Enter the Price of Book: ");
         double book = sc.nextDouble();
 
-        double totalCostWithoutGst= pen+pencil+book;
+        // Calculate total cost without GST
+        double totalCostWithoutGst = pen + pencil + book;
+        System.out.println("Total Cost without GST: " + totalCostWithoutGst);
 
-         System.out.println("Total Cost without Gst"+ totalCostWithoutGst);
+        // Calculate GST (18%) and total cost with GST
+        double gst = 0.18 * totalCostWithoutGst;
+        System.out.println("GST Added for 18%: " + gst);
 
-         double GstAddedOnPrice= 0.18*totalCostWithoutGst;
+        double totalCostWithGst = totalCostWithoutGst + gst;
+        System.out.println("Total Bill Amount: " + totalCostWithGst);
 
-         System.out.println("Gst Added for 18% "+ GstAddedOnPrice);
-
-         double totalCostWithGstAdded = totalCostWithoutGst+ GstAddedOnPrice;
-
-         System.out.println("Total Bill Amount :"+ totalCostWithGstAdded);
-        sc.close();
-
+        sc.close();  // Close Scanner
     }
 }
+`

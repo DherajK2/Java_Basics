2ded33e7d8f9ec52d47518a8331bb3fcc29c796b
DherajK2 <dherajkd2@gmail.com>
2026-01-16 15:30:01 +0530

    Day 2 Completed

---
+++ Day02/TernaryOp1.java
@@ -0,0 +1,10 @@
+public class TernaryOp1 {
+    public static void main(String[] args) {
+        int a =9;
+        int b=8;
+
+        b = (a>5) ? 35:7;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Day02/TernaryOp2MaxNum.java
@@ -0,0 +1,10 @@
+
+public class TernaryOp2MaxNum {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+
+        int max= (a>b)? a:b ;
+        System.out.println("The maximum Number from 2 nums is :"+max);
+    }
+}
